5bf9099177997fa3946ace04ae974ed428a1c823
Jad Raad <jad.d.raad@gmail.com>
2023-07-08 10:17:38 -0400

    About done with classification, on to seg

--- train/model_architectures.py
+++ train/model_architectures.py
@@ -355,3 +355,34 @@ class MinierUNet(nn.Module):
         x5 = self.output(x4)
         
         return x5
+    
+class BinaryClassifier(nn.Module):
+    """Relatively simple feature extraction and binary classification."""
+    def __init__(self):
+        super(BinaryClassifier, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(256 * 14 * 14, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 1),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
